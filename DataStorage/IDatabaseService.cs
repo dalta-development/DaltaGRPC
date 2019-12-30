facad9dd776aaf0f2bd5c4fced34f4762bcbddf9
Dukeeeee <40759437+GithubDuke@users.noreply.github.com>
2019-12-30 12:39:35 +0100

    Made the DataStorage asynchronous

--- DataStorage/IDatabaseService.cs
+++ DataStorage/IDatabaseService.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace DataStorage
 {
-    public class Class1
+    public interface IDatabaseService
     {
-
+        Task<List<T>> Store<T>(List<T> items);
+        Task<T> FindOne<T>(Expression<Func<T, bool>> predicate);
+        Task<T> UpdateOne<T>(Expression<Func<T, bool>> predicate, T item);
+        Task<T> DeleteOne<T>(T item);
+        Task<List<T>> FindMany<T>(Expression<Func<T, bool>> predicate);
+        Task<List<T>> UpdateMany<T>(Expression<Func<T, bool>> predicate, List<T> items);
+        Task<List<T>> DeleteMany<T>(Expression<Func<T, bool>> predicate, List<T> items);
     }
 }
--- DataStorage/MongoDataService.cs
+++ DataStorage/MongoDataService.cs
@@ -3,46 +3,60 @@ using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DataStorage
 {
     class MongoDataService : IDatabaseService
     {
-        MongoClient client = new MongoClient("mongodb://localhost");
-        IMongoDatabase db = 
+        private readonly MongoClient _client;
+        private readonly IMongoDatabase _db;
 
-        public List<T> DeleteMany<T>(Expression<Func<T, bool>> predicate, List<T> items)
+        public MongoDataService()
         {
+            _client = new MongoClient("mongodb://localhost:27017");
+            _db = _client.GetDatabase("DaltaDB");
+        }
+
+        Task<List<T>> IDatabaseService.DeleteMany<T>(Expression<Func<T, bool>> predicate, List<T> items)
+        {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public T DeleteOne<T>(T item)
+        Task<T> IDatabaseService.DeleteOne<T>(T item)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public List<T> FindMany<T>(Expression<Func<T, bool>> predicate)
+        Task<List<T>> IDatabaseService.FindMany<T>(Expression<Func<T, bool>> predicate)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public T FindOne<T>(Expression<Func<T, bool>> predicate)
+        Task<T> IDatabaseService.FindOne<T>(Expression<Func<T, bool>> predicate)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public List<T> Store<T>(List<T> items)
+        Task<List<T>> IDatabaseService.Store<T>(List<T> items)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public List<T> UpdateMany<T>(Expression<Func<T, bool>> predicate, List<T> items)
+        Task<List<T>> IDatabaseService.UpdateMany<T>(Expression<Func<T, bool>> predicate, List<T> items)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
 
-        public T UpdateOne<T>(Expression<Func<T, bool>> predicate, T item)
+        Task<T> IDatabaseService.UpdateOne<T>(Expression<Func<T, bool>> predicate, T item)
         {
+            var collection = _db.GetCollection<T>(nameof(T));
             throw new NotImplementedException();
         }
     }
