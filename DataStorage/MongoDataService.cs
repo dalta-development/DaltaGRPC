2d5a40ceaff58859708b8176cad24caaf51af5f3
Dukeeeee <40759437+GithubDuke@users.noreply.github.com>
2020-01-04 14:53:30 +0100

    + Added IdentityServer project + Generated stubs for grade.proto + Testing IdentityServer

--- DataStorage/MongoDataService.cs
+++ DataStorage/MongoDataService.cs
@@ -15,6 +15,7 @@ namespace DataStorage
         public MongoDataService()
         {
             _client = new MongoClient("mongodb://localhost:27017");
+            _db = _client.GetDatabase("DaltaDB");
         }
 
         Task<bool> SchoolExists(string schoolUUID)
@@ -30,50 +31,49 @@ namespace DataStorage
             }
         }
 
-        Task<long> IDatabaseService.DeleteMany<T>(Expression<Func<T, bool>> predicate, string schoolUUID)
+        Task<long> IDatabaseService.DeleteMany<T>(Expression<Func<T, bool>> predicate)
         {
-            _db = _client.GetDatabase(schoolUUID);
             var collection = _db.GetCollection<T>(typeof(T).Name);
             var result = collection.DeleteMany<T>(predicate).DeletedCount;
             return Task.FromResult(result);
         }
 
-        Task<string> IDatabaseService.DeleteOne<T>(Expression<Func<T, bool>> predicate, string schoolUUID)
+        Task<string> IDatabaseService.DeleteOne<T>(Expression<Func<T, bool>> predicate)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             var result = collection.DeleteOneAsync<T>(predicate);
             return Task.FromResult(result.Result.ToString());
         }
 
-        async Task<List<T>> IDatabaseService.FindMany<T>(Expression<Func<T, bool>> predicate, string schoolUUID)
+        async Task<List<T>> IDatabaseService.FindMany<T>(Expression<Func<T, bool>> predicate)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             var result = await collection.Find<T>(predicate).ToListAsync();
             return result;
         }
 
-        async Task<T> IDatabaseService.FindOne<T>(Expression<Func<T, bool>> predicate, string schoolUUID)
+        async Task<T> IDatabaseService.FindOne<T>(Expression<Func<T, bool>> predicate)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             var result = await collection.Find<T>(predicate).SingleAsync();
             return result;
         }
 
-        async Task<List<T>> IDatabaseService.StoreMany<T>(List<T> items, string schoolUUID)
+        async Task<List<T>> IDatabaseService.StoreMany<T>(List<T> items)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             await collection.InsertManyAsync(items);
             return items;
         }
 
-        async Task<T> IDatabaseService.StoreOne<T>(T item, string schoolUUID)
+        async Task<T> IDatabaseService.StoreOne<T>(T item)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             await collection.InsertOneAsync(item);
             return item;
         }
 
-        async Task<List<T>> IDatabaseService.UpdateMany<T>(Expression<Func<T, bool>> predicate, List<T> items, string schoolUUID)
+        async Task<List<T>> IDatabaseService.UpdateMany<T>(Expression<Func<T, bool>> predicate, List<T> items)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             items.ForEach(x =>
@@ -83,7 +83,7 @@ namespace DataStorage
             return items;
         }
 
-        async Task<T> IDatabaseService.UpdateOne<T>(Expression<Func<T, bool>> predicate, T item, string schoolUUID)
+        async Task<T> IDatabaseService.UpdateOne<T>(Expression<Func<T, bool>> predicate, T item)
         {
             var collection = _db.GetCollection<T>(typeof(T).Name);
             await collection.ReplaceOneAsync(predicate, item);
